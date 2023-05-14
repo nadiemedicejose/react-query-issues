312a5e3ebf775be0f4357922b9aae3aa97919d30
Jose Estrada <jose.estrada@sonorasoft.com>
2023-05-14 14:50:11 -0700

    Axios y Tiipo de dato Label

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,13 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
+import { githubApi } from '../../api/githubApi';
+import { Label } from '../interfaces/label';
 
-const getLabels = async() => {
-  const res = await fetch('https://api.github.com/repos/facebook/react/labels');
-  const data = await res.json();
-
-  console.log(data);
+const getLabels = async(): Promise<Label[]> => {
+  const { data } = await githubApi.get<Label[]>('/labels');
   return data;
 }
-
 
 export const LabelPicker = () => {
   const labelsQuery = useQuery(
