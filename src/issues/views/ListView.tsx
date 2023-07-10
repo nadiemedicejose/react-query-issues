a645ead446b24e706f09607735c9c2d2f2e87408
Jose Estrada <jose.estrada@sonorasoft.com>
2023-07-09 17:55:04 -0700

    Mostrar issues en pantalla

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
 import { useIssues } from '../hooks/useIssues';
+import { LoadingIcon } from '../../shared/components/LoadingIcon';
 
 
 export const ListView = () => {
@@ -17,7 +18,7 @@ export const ListView = () => {
     <div className="row mt-5">
       
       <div className="col-8">
-        <IssueList />
+        {issuesQuery.isLoading ? <LoadingIcon /> : <IssueList issues={issuesQuery.data ?? []} />}
       </div>
       
       <div className="col-4">
