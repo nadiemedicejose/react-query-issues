e74465a2fb93a5eaa59f46769e2479e773fb7b5e
Jose Estrada <jose.estrada@sonorasoft.com>
2023-11-19 22:53:54 -0700

    Filtrar issues: Open y Closed

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,19 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { githubApi } from "../../api/githubApi";
-import { Issue } from "../interfaces";
+import { Issue, State } from "../interfaces";
 import { sleep } from "../../helpers/sleep";
 
-const getIssues = async(): Promise<Issue[]> => {
+interface Props {
+    state?: State;
+    labels: string[];
+}
+
+const getIssues = async(labels: string[], state?: State): Promise<Issue[]> => {
     await sleep(2);
-    const { data } = await githubApi.get<Issue[]>("/issues");
+    
+    const params = new URLSearchParams();
+    if (state) params.append('state', state);
+
+    const { data } = await githubApi.get<Issue[]>("/issues", {params});
     return data;
 }
 
 
-export const useIssues = () => {
+export const useIssues = ({state, labels}: Props) => {
     const issuesQuery = useQuery(
-        ["issues"],
-        getIssues,
+        ["issues", {state, labels}],
+        () => getIssues(labels, state),
     );
 
     return {
